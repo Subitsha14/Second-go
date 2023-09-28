e26e5701b43223dc71d50a0b359f851a861d1555
Subitsha <subikarthika14@gmail.com>
2023-09-28 21:48:16 +0530

    final commit

--- Greetings/greetings.go
+++ Greetings/greetings.go
@@ -15,8 +15,21 @@ func Hello(name string) (string, error) {
 	return greet, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 func randomFormat() string {
-	formats := []string{"Hii %v. Wecome!", "Great to see you, %v", "Hail, %v! well met!"}
+	formats := []string{"Hii %v. Wecome! \n", "Great to see you, %v \n", "Hail, %v! well met! \n"}
 
 	return formats[rand.Intn(len(formats))]
 }
--- Hello/hello.go
+++ Hello/hello.go
@@ -10,7 +10,10 @@ func main() {
 	//caller function
 	log.SetPrefix("Greetings: ")
 	log.SetFlags(0)
-	greet, err := greetings.Hello("subi")
+
+	names := []string{"subi", "sanju", "namjoon", "jin", "suga", "hobi", "tae", "jimin", "kookie"}
+	greet, err := greetings.Hellos(names)
+	//greet, err := greetings.Hello("subi")
 	//greet, err := greetings.Hello("")
 	if err != nil {
 		log.Fatal(err)
